8adb1789c29d41d26dea7fb625dc893ab524d0d8
Luiz Felipe <lfelipe1993@live.com>
2020-04-13 12:39:27 -0300

    obtendo um usuario por Id

--- src/main/java/br/net/digitalzone/workshopmongo/services/UserService.java
+++ src/main/java/br/net/digitalzone/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package br.net.digitalzone.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.net.digitalzone.workshopmongo.domain.User;
 import br.net.digitalzone.workshopmongo.repository.UserRepository;
+import br.net.digitalzone.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
 }
