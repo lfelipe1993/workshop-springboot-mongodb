76a366d0423349bee0f8a2ee7ba1c57768decf41
Luiz Felipe <lfelipe1993@live.com>
2020-04-13 17:20:10 -0300

    Consulta com vários critérios

--- src/main/java/br/net/digitalzone/workshopmongo/resources/PostResource.java
+++ src/main/java/br/net/digitalzone/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package br.net.digitalzone.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,24 @@ public class PostResource {
 		text = URL.decodeParam(text);
 		
 		List<Post> list = service.findByTitle(text);
+
+		return ResponseEntity.ok().body(list);
+
+	}
+	
+	@RequestMapping(value = "/fullsearch",method = RequestMethod.GET) // or @GetMapping
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate
+			
+			) {
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date(0L));
+		
+		List<Post> list = service.fullSearch(text, min, max);
 
 		return ResponseEntity.ok().body(list);
 
